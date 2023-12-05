4fb36050b656b70548aaa04f0087230bf740f0cb
Guillaume Chau <guillaume.b.chau@gmail.com>
2023-12-05 10:28:52 +0100

    feat: ResourceToolbarInfo

---
+++ packages/moquerie/components/resource/ResourceToolbarInfo.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+import type { ResourceSchemaType } from '~/types/resource.js'
+
+defineProps<{
+  type: ResourceSchemaType
+}>()
+</script>
+
+<template>
+  <div>{{ type.name }}</div>
+  <div v-if="type.description" class="text-xs text-gray-500 dark:text-gray-400 flex-shrink truncate">
+    {{ type.description }}
+  </div>
+  <div v-if="type.tags.length" class="flex gap-1">
+    <UBadge
+      v-for="tag in type.tags"
+      :key="tag"
+      variant="soft"
+    >
+      {{ tag }}
+    </UBadge>
+  </div>
+</template>
--- packages/moquerie/pages/db/factories/[resourceName].vue
+++ packages/moquerie/pages/db/factories/[resourceName].vue
@@ -8,19 +8,9 @@ onWindowFocus(refresh)
 <template>
   <div v-if="data" class="flex flex-col divide-y divide-gray-300 dark:divide-gray-700">
     <div class="px-2 py-1 flex items-center gap-2">
-      <div>{{ data.name }}</div>
-      <div class="text-xs text-gray-500 dark:text-gray-400 flex-shrink truncate">
-        {{ data.description }}
-      </div>
-      <div class="flex gap-1">
-        <UBadge
-          v-for="tag in data.tags"
-          :key="tag"
-          variant="soft"
-        >
-          {{ tag }}
-        </UBadge>
-      </div>
+      <ResourceToolbarInfo
+        :type="data"
+      />
 
       <div class="flex-1" />
 
