5b15fb45cfc06b52a61bd4611fb9f278094a01b0
Guillaume Chau <guillaume.b.chau@gmail.com>
2024-01-02 11:29:42 +0100

    perf: cache env utils

--- packages/core/src/util/env.ts
+++ packages/core/src/util/env.ts
@@ -1,18 +1,29 @@
 import process from 'node:process'
 import fs from 'node:fs'
 import path from 'pathe'
+import { LRUCache } from 'lru-cache'
 import { findUp } from './find-up.js'
 
 export function getCwd() {
   return path.resolve(process.env.MOQUERIE_OVERRIDE_CWD ?? process.cwd())
 }
 
+const projectNameCache = new LRUCache<string, string>({
+  max: 100,
+  ttl: 5000,
+})
+
 export function getProjectName(): string {
   const cwd = getCwd()
+  const cached = projectNameCache.get(cwd)
+  if (cached) {
+    return cached
+  }
   const pkgFile = findUp(cwd, ['package.json'])
   if (pkgFile) {
     try {
       const pkg = JSON.parse(fs.readFileSync(pkgFile, 'utf-8'))
+      projectNameCache.set(cwd, pkg.name)
       return pkg.name
     }
     catch (e: any) {
@@ -22,8 +33,18 @@ export function getProjectName(): string {
   throw new Error(`[moquerie] package.json not found looking from ${cwd}`)
 }
 
+const projectHasTypescriptCache = new LRUCache<string, boolean>({
+  max: 100,
+  ttl: 1000 * 60 * 5,
+})
+
 export function projectHasTypescript(): boolean {
   const cwd = getCwd()
+  const cached = projectHasTypescriptCache.get(cwd)
+  if (cached !== undefined) {
+    return cached
+  }
   const tsconfigFile = findUp(cwd, ['tsconfig.json'])
+  projectHasTypescriptCache.set(cwd, !!tsconfigFile)
   return !!tsconfigFile
 }
