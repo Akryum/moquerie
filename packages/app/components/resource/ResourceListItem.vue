b4b6d98ed7d2751b1f747224f7c2d22c5bd9b5dc
Guillaume Chau <guillaume.b.chau@gmail.com>
2024-01-03 11:08:22 +0100

    fix: wrong import

--- packages/app/components/resource/ResourceListItem.vue
+++ packages/app/components/resource/ResourceListItem.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import type { ResourceSchemaType } from '~/types'
+import type { ResourceSchemaType } from '@moquerie/core'
 
 const props = defineProps<{
   resourceType: ResourceSchemaType
