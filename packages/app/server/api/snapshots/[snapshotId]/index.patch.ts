ecb4a79176ec63718fe9bdbd8dd691c1eb4eaa24
Guillaume Chau <guillaume.b.chau@gmail.com>
2024-01-17 17:12:09 +0100

    fix(snapshot): move resources when changing location

--- packages/app/server/api/snapshots/[snapshotId]/index.patch.ts
+++ packages/app/server/api/snapshots/[snapshotId]/index.patch.ts
@@ -1,5 +1,8 @@
-import { getSnapshotStorage } from '@moquerie/core'
+import fs from 'node:fs'
+import path from 'pathe'
+import { getSnapshotFolder, getSnapshotStorage } from '@moquerie/core'
 import SuperJSON from 'superjson'
+import { copyDir, ensureDir } from '@moquerie/core/util'
 
 export default defineEventHandler(async (event) => {
   const { id, location, description, tags } = await readBody(event)
@@ -8,11 +11,36 @@ export default defineEventHandler(async (event) => {
   if (!snapshot) {
     throw new Error(`Snapshot ${id} not found`)
   }
+
+  // Copy resources
+  let foldersToCopy: string[] = []
+  if (snapshot.location !== location) {
+    const folder = await getSnapshotFolder(snapshot)
+    foldersToCopy = (await fs.promises.readdir(folder))
+      .filter(file => file !== 'snapshot.json')
+      .map(file => path.join(folder, file))
+      .filter(file => fs.statSync(file).isDirectory())
+  }
+
+  // Update in storage
+
   Object.assign(snapshot, {
     location,
     description,
     tags,
   })
   await storage.save(snapshot)
+
+  // Copy resources
+  if (foldersToCopy.length) {
+    const folder = await getSnapshotFolder(snapshot)
+    await Promise.all(foldersToCopy.map((file) => {
+      const dest = path.join(folder, path.basename(file))
+      return copyDir(file, dest)
+    }))
+
+    await Promise.all(foldersToCopy.map(file => fs.promises.rm(file, { recursive: true })))
+  }
+
   return SuperJSON.stringify(snapshot)
 })
